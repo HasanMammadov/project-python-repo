e3cd60f3e462f577c5bbc833ef681edd9515aefd
HasanMammadov <hasanmamedoff@ghmail.com>
2022-11-17 20:18:53 -0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def divide(num1, num2):
+    # return value num1/num2
+    if num2 == 0:
+        ans = 0
+    else:
+        ans = num1 / num2
+
+    return ans        
+
+myasnwer = divide(15,5)
+print(f'My answer is {myasnwer}')
+
+myasnwer = divide(10,5)
+print(f'My answer is {myasnwer}')
