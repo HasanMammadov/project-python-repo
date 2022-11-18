8f7acddfdb065d99b4ec84eed73e9d735406ec80
HasanMammadov <hasanmamedoff@ghmail.com>
2022-11-17 19:35:59 -0500

    functuion1

---
+++ functions.py
@@ -0,0 +1,16 @@
+# age = input (('Enter Your Age: '))
+# print(age)
+
+# city = input('Enter Your city: ')
+# print(city)
+
+# food = input('Enter Your food: ')
+# print(food)
+
+def myprint(userinput):
+    userinput = input(f'Enter your {userinput}: ')
+    print(userinput)
+
+myprint('name')
+myprint('city')
+myprint('food')
