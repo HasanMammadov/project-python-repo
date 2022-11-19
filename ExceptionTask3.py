2a197fb20b072726d6251b0c7edd9770e7f495ef
HasanMammadov <hasanmamedoff@ghmail.com>
2022-11-19 13:00:15 -0500

    final version

--- ExceptionTask3.py
+++ ExceptionTask3.py
@@ -13,16 +13,16 @@
 fruits = ["banana", "mango", "pear", "apple", "kiwi", "grape"]
 count = 3
 while count > 0:
-	try:
-		index = int(input("Pick an index number to choose your favorite fruit: "))
-		print(f"My favorite fruit is: ", fruits[index])
-	except IndexError:
-		count -= 1
-		print(f"There is no such an index. You have {count} right left. Try again!")
-	except ValueError:
-		print(f"You should enter integer. You have {count} right left. Try again!")
-	else:
-		print("Congrats! You've entered a valid input.")
-		break
-	finally:
-		print("Our fruits are always fresh!")
+    try:
+        index = int(input("Pick an index number to choose your favorite fruit: "))
+        print("My favorite fruit is: ", fruits[index])
+    except IndexError:
+        count -= 1
+        print(f"There is no such an index. You have {count} right left. Try again!")
+    except ValueError:
+        print(f"You should enter integer. You have {count} right left. Try again!")
+    else:
+        print(f"Congrats! You've entered a valid input")
+        break
+    finally:
+        print("Our fruits are always fresh!")
