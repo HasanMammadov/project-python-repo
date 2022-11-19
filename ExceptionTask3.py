bdfb539338394b5e7eefad1f06a259eec823829f
HasanMammadov <hasanmamedoff@ghmail.com>
2022-11-19 12:56:50 -0500

    whileLoopException

--- ExceptionTask3.py
+++ ExceptionTask3.py
@@ -1,10 +1,28 @@
+# fruits = ["banana", "mango", "pear", "apple", "kiwi", "grape"]
+# while True:
+# 	try:
+# 		index = int(input("Pick an index number to choose your favorite fruit: "))
+# 		print("My favorite fruit is", fruits[index])
+# 		break
+# 	except IndexError:
+# 		print("There is no such an index. Try again!")
+# 	except ValueError:
+# 		print("You should enter integer. Try again!")
+
+
 fruits = ["banana", "mango", "pear", "apple", "kiwi", "grape"]
-while True:
+count = 3
+while count > 0:
 	try:
-		index = int(input("Pick an index number to choose your favorite fruit: "))
+		index = int(input("Pick an index number to choose your favorite fruit"))
 		print("My favorite fruit is", fruits[index])
-		break
 	except IndexError:
-		print("There is no such an index. Try again!")
+		count -= 1
+		print("There is no such an index. You have {count} right left. Try again!")
 	except ValueError:
-		print("You should enter integer. Try again!")
+		print("You should enter integer. You have {count} right left. Try again!")
+	else:
+		print("Congrats! You've entered a valid input.")
+		break
+	finally:
+		print("Our fruits are always fresh!")
